8c61f56fe10e90827d7798da47a06f8bde550aa6
mohdbazlur <76091637+mohdbazlur@users.noreply.github.com>
2021-02-09 09:53:34 +0530

    Update add_with_4_functions.c

--- add_with_4_functions.c
+++ add_with_4_functions.c
@@ -2,28 +2,28 @@
 #include<stdio.h>
 float input()
 {
-    float num;
-    printf("Enter the number:");
-    scanf("%f",&num);
-    return num ;
+			float num;
+			printf("Enter the number:\n");
+			scanf("%f",&num);
+			return num ;
 }
-    float sum(float num1,float num2)
+			float sum(float num1,float num2)
 {
-    float sum;
-    sum=num1+num2;
-    return sum;
+			float sum;
+			sum=num1+num2;
+			return sum;
 }
-    void output(float x,float y,float num)
+			void output(float x,float y,float num)
 {
-    printf("Sum of %f and %f is  %f",x,y,num);
-    return 0;
+			printf("Sum of %f and %f is  %f\n",x,y,num);
+			return 0;
 }
-    int main()
+			int main()
 {
-    float a,b,s;
-    a=input();
-    b=input();
-    s=sum(a,b);
-    output(a,b,s);
-    return 0;
+			float a,b,s;
+			a=input();
+			b=input();
+			s=sum(a,b);
+			output(a,b,s);
+			return 0;
 }
