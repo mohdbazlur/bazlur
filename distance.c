a6eaf606654cec371432761e1e126947daa0ad1b
mohdbazlur <76091637+mohdbazlur@users.noreply.github.com>
2021-02-10 18:38:40 +0530

    Update distance.c

--- distance.c
+++ distance.c
@@ -3,29 +3,29 @@
 #include <math.h>
 float input()
 {
-							float num;
-							printf("Enter the point x1, x2, y1, y2 respectively:");
-							scanf("%f",&num);
-							return num ;
+				float num;
+				printf("Enter the point x1, x2, y1, y2 respectively:");
+				scanf("%f",&num);
+				return num ;
 }
 float distance(float x1,float x2, float y1,float y2)
 {
-							float distance;
-							distance= sqrt((x2-x1)*(x2-x1))+((y2-y1)*(y2-y1));
-							return distance;
+				float distance;
+				distance= sqrt((x2-x1)*(x2-x1))+((y2-y1)*(y2-y1));
+				return distance;
 }
 float output(float distance)
 {
-							printf("distance between two points is  %f",sqrt(distance));
+				printf("The distance between %f,%f and %f,%f is %f\n",x1,y1,x2,y2,sqrt(distance));
 }
 int main()
 {
-							float x1,x2,y1,y2,d;
-							x1=input();
-							x2=input();
-							y1=input();
-							y2=input();
-							d=distance(x1,x2,y1,y2);
-							output(d);
-							return 0;
+				float x1,x2,y1,y2,d;
+				x1=input();
+				x2=input();
+				y1=input();
+				y2=input();
+				d=distance(x1,x2,y1,y2);
+				output(d);
+				return 0;
 }
