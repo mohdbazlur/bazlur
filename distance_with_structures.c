4fb7ca5f9f0353d70557ec6c31045fac8e79944b
mohdbazlur <76091637+mohdbazlur@users.noreply.github.com>
2021-02-14 21:24:02 +0530

    Update distance_with_structures.c

--- distance_with_structures.c
+++ distance_with_structures.c
@@ -3,37 +3,37 @@
 #include <math.h>
 struct point
 {
-    float x;
-    float y;
+		float x;
+		float y;
 };
 typedef struct point Point;
 Point input()
 {
-    Point a;
-    printf("Enter the value of the coordinate x % lf: ");
-    scanf("%f",&a.x);
-    printf("Enter the value of the coordinate y % lf:");
-    scanf("%f",&a.y);
-    return a;
+		Point a;
+		printf("Enter the value of the coordinate x % lf: ");
+		scanf("%f",&a.x);
+		printf("Enter the value of the coordinate y % lf:");
+		scanf("%f",&a.y);
+		return a;
 }
 float compute(Point a1,Point a2)
 {
-    float distance;
-    distance=sqrt(pow((a1.x-a2.x),2)+pow((a1.y-a2.y),2));
-    return distance;
+		float distance;
+		distance=sqrt(pow((a1.x-a2.x),2)+pow((a1.y-a2.y),2));
+		return distance;
 }
 void output(Point a1, Point a2, float dist)
 {
-    printf("The distance between %f,%f and %f,%f is %f",a1.x,a1.y,a2.x,a2.y,dist);
+		printf("The distance between %f,%f and %f,%f is %f",a1.x,a1.y,a2.x,a2.y,dist);
 }
 int main(void)
 {
-    float dist;
-    Point a1,a2;
-    a1=input();
-    a2=input();
-    dist=compute(a1,a2);
-    output(a1, a2, dist);
-    return 0;
+		float dist;
+		Point a1,a2;
+		a1=input();
+		a2=input();
+		dist=compute(a1,a2);
+		output(a1, a2, dist);
+		return 0;
 }
 
